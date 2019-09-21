db0777eb879c7159b7b974cea4ba52a07ef8edb1
BatistaJeff <trabalhojbm@gmail.com>
2019-09-20 21:58:43 -0300

    commit final

---
+++ Demo09/src/main/java/com/demo/Demo09Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo09Application.class, args);
+	}
+
+}
